7a14006463e32b5ed75e9cb543e342ec166082fc
joq62 <joq62@c50>
2024-03-22 02:45:49 +0100

    created for testing purpose

---
+++ deployment_specs_check.erl
@@ -0,0 +1,49 @@
+-module(deployment_specs_check).
+
+-export([
+	 start/0,
+	 %% Support
+	 all_files/0,
+	 all_info/0
+	]).
+
+
+-define(Dir,".").
+-define(FileExt,".deployment").
+-define(MapKeys,[deployments,id]).
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+    
+    check(all_info()),
+    init:stop(),
+    ok.
+
+check([])->
+    io:format("Success, OK ! ~n");
+check([{ok,[Map]}|T])->
+    io:format("Checking ~p~n",[Map]),
+
+    ?MapKeys=lists:sort(maps:keys(Map)),
+    check(T).
+
+   
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+all_files()->
+    {ok,Files}=file:list_dir(?Dir),
+    FileNames=[filename:join(?Dir,Filename)||Filename<-Files,
+					     ?FileExt=:=filename:extension(Filename)],
+    FileNames.    
+all_info()->
+    [file:consult(File)||File<-all_files()].
+	    
+    
